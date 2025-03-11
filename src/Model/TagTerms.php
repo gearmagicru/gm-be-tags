5a0d5c805400c5ff836a10adab75d9338f408069
Anton Tivonenko <93121899+gearmagicru-ti@users.noreply.github.com>
2025-03-11 16:15:11 +0300

    Добавлена акт. запись меток  терминов TagTerms.php
    Добавлена класс активной записи меток  (тегов) терминов Gm\Backend\Tags\Model\TagTerms

---
+++ src/Model/TagTerms.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+namespace Gm\Backend\Tags\Model;
+
+/**
+ * Активная запись меток (тегов) терминов.
+ * 
+ * @author Anton Tivonenko <anton.tivonenko@gmail.com>
+ * @package Gm\Backend\Tags\Model
+ * @since 1.0
+ */
+class TagTerms extends \Gm\Tagger\TagTerms
+{
+}
