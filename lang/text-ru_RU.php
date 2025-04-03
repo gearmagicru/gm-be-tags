1e86a81687c4f17ab292860f6f0b4bf498ec965c
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-04-03 20:49:22 +0300

    Обновлены локализации

--- lang/text-ru_RU.php
+++ lang/text-ru_RU.php
@@ -35,18 +35,17 @@ return [
     'A slug is a version of the title, a unique part of the URL. It is all lowercase and only Latin letters, numbers, and hyphens. If not specified, it will be created automatically from the name.' =>
         'Cлаг - это версия названия, уникальная часть URL-адреса. Это все строчные буквы и только буквы на латинице, цифры и дефисы. Если не указан, он будет создан автоматически из названия.',
     'Language' => 'Язык',
-    'Hits' => 'Просмотры',
-    'Count of views' => 'Количество просмотров',
+    'Hits' => 'Переходы',
+    'Count of hits' => 'Количество переходов',
     'Count of articles containing the tag' => 'Количество материала содержащий метку',
     'Yes' => 'да',
     'No' => 'нет',
     'View tag' => 'Просмотреть страницу метки',
-    'Visible' => 'Видимый',
-    'Show / hide tag' => 'Показать / скрыть метку',
+    'Visibility' => 'Видимость',
     // Grid: сообщения
     'Tag «{0}» - showen' => 'Метка «{0}» - <b>показана</b>.',
     'Tag «{0}» - hidden' => 'Метка «{0}» - <b>скрыта</b>.',
     // Grid: сообщения / заголовки
-    'Show' => 'Отобразить',
+    'Show' => 'Показать',
     'Hide' => 'Скрыть'
 ];
